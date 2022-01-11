e15580105a8365e449a8fc6d8b78e15bde560233
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2022-01-10 21:47:12 -0300

    Incluindo os calculos.

--- app/src/main/java/br/eti/inovare/calculadoradegorjeta/MainActivity.java
+++ app/src/main/java/br/eti/inovare/calculadoradegorjeta/MainActivity.java
@@ -3,12 +3,61 @@ package br.eti.inovare.calculadoradegorjeta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editValor;
+    private TextView textPorcentagem, textGorjeta, textTotal;
+    private SeekBar seekBarGorjeta;
+    private double porocentagem = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editValor = findViewById(R.id.editValor);
+        textGorjeta = findViewById(R.id.textGorjeta);
+        textPorcentagem = findViewById(R.id.textPorcentagem);
+        textTotal = findViewById(R.id.textTotal);
+        seekBarGorjeta = findViewById(R.id.seekBarGorjeta);
+
+        seekBarGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                porocentagem = i;
+                textPorcentagem.setText(Math.round(porocentagem) + " %");
+                calcular();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void calcular() {
+        String valorUsuario = editValor.getText().toString();
+
+        if (valorUsuario == null || valorUsuario.equals("")) {
+            Toast.makeText(getApplicationContext(), "Digite um valor primeiro!", Toast.LENGTH_LONG).show();
+        } else {
+            double valor = Double.parseDouble(valorUsuario);
+            double gorjeta = valor * (porocentagem / 100);
+            double total = valor + gorjeta;
+
+            textGorjeta.setText("R$ " + gorjeta);
+            textTotal.setText("R$ " + total);
+        }
     }
 }
